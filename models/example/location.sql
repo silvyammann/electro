79ead79e5855853863d3f0b46801bb02c6ee6c24
silvyammann <85229887+silvyammann@users.noreply.github.com>
2022-07-19 18:37:54 +0200

    Delete location.sql

--- models/example/location.sql
+++
@@ -1,7 +0,0 @@
-with operators as (
-    select evse_data->'EVSEDataRecord' as datarecord, id
-    from {{ ref('operators') }}
-)
-select (locations.location_data->'EvseID')::text as id, operators.id as operator_id, locations.location_data as location_data
-from operators
-cross join lateral (select json_array_elements(operators.datarecord::json) as location_data) as locations
